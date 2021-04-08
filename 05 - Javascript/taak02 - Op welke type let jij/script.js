46b1a12d9e70cec195386fcac83be6be8f90a2b0
David van Oudheusden <d.vanoudheusden@rocva.nl>
2021-04-08 12:22:02 +0200

    console gebouwd rond variabelen.

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,18 @@
+
+
+let voornaam = "David";
+let achternaam = "van Oudheusden";
+let leeftijd = 42;
+let ikBenStemGerechtig = true;
+let ikMagLegaalAlcoholKopen = true;
+let ikMagBrommerRijden = true;
+let SoftwareOntwikkeling = null;
+
+
+
+leeftijd = leeftijd - 12;
+
+console.log(leeftijd);
+
+
+
